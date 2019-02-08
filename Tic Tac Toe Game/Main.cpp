2977692865f72a0b1346962851311c8237a1eabe
Nicole Dance <nicoledance@chaosshard.com.au>
2019-02-08 14:58:44 +1100

    Game Loop and menu set up, and basic loop going through the 9 turns in place as well.

--- Tic Tac Toe Game/Main.cpp
+++ Tic Tac Toe Game/Main.cpp
@@ -13,14 +13,16 @@ int main() {
 		std::cout << "   TT    TT  TT            TT      TTTTTT  TT           TT     TT  TT  TT" << std::endl;
 		std::cout << "   TT    TT  TTTTTT        TT      TT  TT  TTTTTT       TT     TTTTTT  TTTTTT" << std::endl;
 
-		std::cout << std::endl << "Would you like to continue? " << std::endl;
+		std::cout << std::endl << "Would you like to play a game? " << std::endl;
 		std::cout << "1. Yes " << std::endl;
 		std::cout << "2. No" << std::endl << "-> ";
 
+		// Retreive selection menu selection input from plater and determine action
 		int selection = 0;
 		std::cin >> selection;
 
-		if (selection == 1)		{
+		if (selection == 1)		
+		{
 			std::cout << std::endl << "Starting game...." << std::endl << std::endl;
 
 			// stop on end game condition or no more moves
@@ -28,10 +30,13 @@ int main() {
 			{
 				std::cout << std::endl << "Turn "<< i << "..." << std::endl;
 			}
-		}else if (selection == 2)		{
-			std::cout << "Thanks for playing. GOOD BYE!" << std::endl;
+			std::cout << std::endl ;
+		}else if (selection == 2)
+		{
+			std::cout << std::endl << "Thanks for playing. GOOD BYE!" << std::endl;
 			playOn = false;
-		}else 		{
+		}else 
+		{
 			std::cout << "Invalid menu selection, please try again." << std::endl;
 		}
 		
