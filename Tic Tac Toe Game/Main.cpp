9c7b3b626cc4cc2e2a295a2258182b532a24520d
Nicole Dance <nicoledance@chaosshard.com.au>
2019-02-08 14:51:47 +1100

    I tihnk its merging wrong lets check...

--- Tic Tac Toe Game/Main.cpp
+++ Tic Tac Toe Game/Main.cpp
@@ -2,30 +2,40 @@
 
 int main() {
 	
-	bool restartMenu = true;
+	bool playOn = true;
 
-	while (restartMenu) {
-		
-		for (int i = 0; i < 11; i++)
-		{
-			std::cout << i << "..." << std::endl;
-		}
+	// Game Loop  Which exits if players do not want to play any more
+	while (playOn) {
 
-		std::cout << "Would you like to continue? " << std::endl;
+		std::cout << "TTTTTTTT TT  TTTTTT     TTTTTTTT     TT    TTTTTT    TTTTTTTT  TTTTTT  TTTTTT" << std::endl;
+		std::cout << "   TT    TT  TT            TT       TTTT   TT           TT     TT  TT  TT" << std::endl;
+		std::cout << "   TT    TT  TT            TT      TT  TT  TT           TT     TT  TT  TTTTTT" << std::endl;
+		std::cout << "   TT    TT  TT            TT      TTTTTT  TT           TT     TT  TT  TT" << std::endl;
+		std::cout << "   TT    TT  TTTTTT        TT      TT  TT  TTTTTT       TT     TTTTTT  TTTTTT" << std::endl;
+
+		std::cout << std::endl << "Would you like to continue? " << std::endl;
 		std::cout << "1. Yes " << std::endl;
-		std::cout << "2. No" << std::endl;
+		std::cout << "2. No" << std::endl << "-> ";
 
 		int selection = 0;
 		std::cin >> selection;
 
 		if (selection == 1)		{
-			std::cout << selection << " Pressed" << std::endl;
+			std::cout << std::endl << "Starting game...." << std::endl << std::endl;
+
+			// stop on end game condition or no more moves
+			for (int i = 1; i < 10; i++)
+			{
+				std::cout << std::endl << "Turn "<< i << "..." << std::endl;
+			}
 		}else if (selection == 2)		{
-			std::cout << selection << " Pressed" << std::endl;
-			restartMenu = false;
+			std::cout << "Thanks for playing. GOOD BYE!" << std::endl;
+			playOn = false;
 		}else 		{
-			std::cout << selection << " Pressed" << std::endl;
+			std::cout << "Invalid menu selection, please try again." << std::endl;
 		}
 		
 	}
+
+
 }
