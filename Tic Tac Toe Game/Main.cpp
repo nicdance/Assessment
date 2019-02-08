7532bb32884410a73f96918922836ec0c6a36e00
Nicole Dance <nicoledance@chaosshard.com.au>
2019-02-08 14:16:06 +1100

    Testing loops and basic menu

--- Tic Tac Toe Game/Main.cpp
+++ Tic Tac Toe Game/Main.cpp
@@ -2,5 +2,30 @@
 
 int main() {
 	
+	bool restartMenu = true;
 
+	while (restartMenu) {
+		
+		for (int i = 0; i < 11; i++)
+		{
+			std::cout << i << "..." << std::endl;
+		}
+
+		std::cout << "Would you like to continue? " << std::endl;
+		std::cout << "1. Yes " << std::endl;
+		std::cout << "2. No" << std::endl;
+
+		int selection = 0;
+		std::cin >> selection;
+
+		if (selection == 1)		{
+			std::cout << selection << " Pressed" << std::endl;
+		}else if (selection == 2)		{
+			std::cout << selection << " Pressed" << std::endl;
+			restartMenu = false;
+		}else 		{
+			std::cout << selection << " Pressed" << std::endl;
+		}
+		
+	}
 }
